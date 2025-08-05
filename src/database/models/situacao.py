369eb47363ccbc141602184f383ba87c4686d2f6
Joao Marcelino <dev.marcelino@hotmail.com>
2025-08-05 19:16:05 -0300

    Implements bahiapilots.com.br scraping

---
+++ src/database/models/situacao.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class Situacao(Enum):
+    PREVISTA = 'PREVISTA'
+    CONFIRMADA = 'CONFIRMADA'
+    ANDAMENTO = 'ANDAMENTO'
+
+
--- src/scrapy.py
+++ src/scrapy.py
@@ -7,26 +7,34 @@ class Scrapy:
         self.cabecalho = []
         self.linhas = []
 
-    def extract(self):
-        headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Referer": "https://www.google.com"
-        }
-        
-        html = requests.get("https://www.sinprapar.com.br/PREV.HTM", headers=headers)
-        
-        if html.status_code != 200 :
-            print(f'Deu ruim')
-            
-        print("Ok")
+    def extract_bahiapilots_com_br(self):
+        soup = self.request_bsoup('https://bahiapilots.com.br/movimentacao-dos-navios/')
+        linhas = soup.find_all("tr")
+        cabecalho = linhas[0]
+        linhas.pop(0)
 
-        soup = BeautifulSoup(html.content, "html.parser")
+        def trata_linhas(item):
+            colunas = BeautifulSoup(str(item), "html.parser")
+            colunas = colunas.find_all("td")
+            colunas = list(map(lambda x: x.get_text(),colunas))
+            return colunas
+
+        linhas = list(map(trata_linhas, linhas))
+        
+        return {
+            'cabecalho': cabecalho,
+            'linhas': linhas
+        }
+            
+
+    def extract_sinprapar_com_br(self):
+
+        soup = self.request_bsoup('https://www.sinprapar.com.br/PREV.HTM')
 
         linhas = soup.find_all("tr")
 
         cabecalho = linhas[1].get_text().split("\n")
-        self.cabecalho = list(filter(lambda x: x != "", cabecalho))
+        cabecalho = list(filter(lambda x: x != "", cabecalho))
 
         linhas.pop(0)
         linhas.pop(0)
@@ -37,7 +45,30 @@ class Scrapy:
             colunas.pop(0)
             return colunas
         
-        self.linhas = list(map(trata_linhas, linhas))
+        linhas = list(map(trata_linhas, linhas))
+
+        return {
+            'cabecalho': cabecalho,
+            'linhas': linhas
+        }
+    
+    def request_bsoup(self, url):
+            
+        headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Referer": "https://www.google.com"
+        }
+        
+        resp = requests.get(url, headers=headers)
+        
+        if resp.status_code != 200 :
+            raise Exception(f'A requisição para {url} falhou.')
+            
+        print(f'Acesso em {url}')
+        soup = BeautifulSoup(resp.content, "html.parser")
+        return soup
+
         
 
 
